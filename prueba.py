672039299cf3c14fb9de076a407d6a3018f4e551
cmorales1 <32424023+cmorales1@users.noreply.github.com>
2018-01-27 12:18:27 -0600

    introduccion

---
+++ prueba.py
@@ -0,0 +1,45 @@
+#metodo de salida#
+print "hola"
+a = 1
+a = 4
+a = 6
+#reasignar datos a variables# 
+b = 2
+c = 3
+d = 4
+e = 5
+#enteros numericos#
+
+f = 0.5
+f = 2.5
+f = 4.5
+#---------------------------#
+g = 1.5
+h = 2.5
+i = 3.5
+j = 4.5
+#Numeros decimales#
+ca = "uno"
+ca = "seis"
+ca = "siete"
+#-------------------#
+cb = "dos"
+cc = "tres"
+cd = "cuatro"
+ce = "cinco"
+#cadenas de texto#
+ab = True
+ac = False
+#verdadero y falso#
+
+
+print a 
+print f 
+print ca 
+print ab 
+print ac
+
+
+
+
+
